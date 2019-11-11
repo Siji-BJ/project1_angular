11c29f3074151d2eb54504bc2c445eb3b2b0314e
Siji B J <siji.bj@experionglobal.com>
2019-11-11 16:42:30 +0530

    form validation is done

--- src/app/add-products/add-products.component.ts
+++ src/app/add-products/add-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnChanges, DoCheck, AfterContentInit  } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ProductService } from '../service/product.service';
 
 // tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
@@ -9,18 +10,30 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AddProductsComponent implements OnInit {
   myForm: FormGroup;
-  constructor() { }
+  constructor(private addProduct: ProductService) { }
   ngOnInit() {
     this.myForm = new FormGroup({
-      name: new FormControl(''),
-      description : new FormControl(''),
-      price : new FormControl(''),
-      image : new FormControl(''),
-      imageAlt : new FormControl(''),
+      name: new FormControl('', Validators.required),
+      description : new FormControl('', [Validators.required, Validators.maxLength(500)] ),
+      price : new FormControl('', Validators.required),
+      image : new FormControl('', Validators.required),
+      imageAlt : new FormControl('', Validators.required),
       isAvailable : new FormControl(''),
     });
   }
-  onSubmit(data) {
-      console.log(data);
+  onSubmit(form: FormGroup) {
+    // console.log(this.myForm.value);
+    console.log('Valid? : "', form.valid , '"' );
+    console.log('Product Name : "', form.value.name, '"' );
+    console.log('Product Description : "', form.value.description,  '"' );
+    console.log('Product Price : "', form.value.price,  '"' );
+    console.log('Product Image : "', form.value.image,  '"' );
+    console.log('Product imageAlt : "', form.value.imageAlt,  '"' );
+    console.log('Product availability : "', form.value.isAvailable,  '"' );
+    if ( form.valid ) {
+      this.addProduct.addProducts(this.myForm.value);
+    } else {
+      alert( ' Not Valid ' );
+    }
   }
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -6,9 +6,9 @@ import { Products } from 'src/app/models/Product';
 export class ProductService {
 
   constructor() { }
-  products: Array <Products> = [
+ private products: Array <Products> = [
     {
-      id: 1,
+      // id: 1,
       name: 'Puma Hat',
       image: 'https://rukminim1.flixcart.com/image/800/960/cap/6/f/f/1-1-84344301-puma-free-original-imae63yzfk9bumvf.jpeg?q=50',
       description: ' Puma hat in black color ',
@@ -17,7 +17,7 @@ export class ProductService {
       price: 5
     },
     {
-      id: 2,
+     // id: 2,
       name: '1164-BR Brown Day and Date Unique New Analog Watch - For Men',
       image: 'https://rukminim1.flixcart.com/image/800/960/jr6o13k0/watch/w/q/z/1164-br-fogg-original-imaf9stmmgg2eq2f.jpeg?q=50',
       description: ' 1164-BR Brown Day and Date Unique New Analog Watch - For Men with gray color ',
@@ -26,7 +26,7 @@ export class ProductService {
       price: 15
     },
     {
-      id: 3,
+      // id: 3,
       name: 'Combo(BR)-1077-349 Sneakers For Men  (Red)',
       // tslint:disable-next-line: max-line-length
       image: 'https://rukminim1.flixcart.com/image/800/960/jnt7low0/shoe/b/7/u/combo-er-1077-349-10-earton-multicolor-original-imafaeq687rqwevd.jpeg?q=50',
@@ -36,7 +36,7 @@ export class ProductService {
       price: 10
     },
     {
-      id: 4,
+     // id: 4,
       name: 'Women Printed A-line Kurta  (Black)',
       // tslint:disable-next-line: max-line-length
       image: 'https://rukminim1.flixcart.com/image/800/960/jyyqc280/kurta/d/w/j/xl-8461759-taavi-original-imafj2mfy6hjyzj9.jpeg?q=50',
@@ -47,7 +47,7 @@ export class ProductService {
     },
 
     {
-      id: 5,
+      // id: 5,
       name: 'Bharat Lifestyle Lexus Fabric 3 + 1 + 1 Dark Grey Sofa Set',
       // tslint:disable-next-line: max-line-length
       image: 'https://rukminim1.flixcart.com/image/416/416/jit64cw0/sofa-set/y/a/b/grey-jute-lexusdrgr311-3-1-1-bharat-lifestyle-dark-grey-original-imaf6gz2hjrbstgs.jpeg?q=70',
@@ -57,7 +57,7 @@ export class ProductService {
       price: 5
     },
     {
-      id: 6,
+      // id: 6,
       name: 'Barbie 6 Pin Eflute MR Bowling',
       // tslint:disable-next-line: max-line-length
       image: 'https://rukminim1.flixcart.com/image/416/416/jiqb8nk0/toy-sport/y/e/h/mbe-sgl100-bowling-barbie-original-imaf6fkfqdzfvdfc.jpeg?q=70',
@@ -67,7 +67,7 @@ export class ProductService {
       price: 15
     },
     {
-      id: 7,
+      // id: 7,
       name: 'CELWARK Metal Bike Keyring For Royal Bikes & Cars Key Chain',
       // tslint:disable-next-line: max-line-length
       image: 'https://rukminim1.flixcart.com/image/416/416/jylvde80/key-chain/d/c/2/metal-bike-keyring-for-royal-bikes-cars-celwark-original-imafgsxkc5gat6mg.jpeg?q=70',
@@ -77,7 +77,7 @@ export class ProductService {
       price: 10
     },
     {
-      id: 8,
+      // id: 8,
       name: 'Noise Shots X-Buds Truly Wireless Bluetooth Headset with Mic  (Teal Green, In the Ear)',
       // tslint:disable-next-line: max-line-length
       image: 'https://rukminim1.flixcart.com/image/416/416/k0e66q80/headphone/n/b/q/noise-shots-x-buds-truly-wireless-original-imafk5fwrfvzz7ux.jpeg?q=70',
@@ -86,9 +86,7 @@ export class ProductService {
       isAvailable: false,
       price: 5
     },
-    {
-      id: 9,
-      name: 'SG Musical Best Quality Tabla Set Tabla',
+    {      name: 'SG Musical Best Quality Tabla Set Tabla',
       // tslint:disable-next-line: max-line-length
       image: 'https://rukminim1.flixcart.com/image/416/416/jhjg13k0/tabla/r/h/n/best-quality-tabla-set-sg-musical-original-imaf5jdw9gdbgesw.jpeg?q=70',
       description: 'SG Musical Best Quality Tabla Set Tabla  (Dayan - 14, Bayan - 22)',
@@ -99,5 +97,9 @@ export class ProductService {
   ];
   getProducts() {
     return this.products;
+  }
+  addProducts(newProduct: Products) {
+         console.log(newProduct);
+         this.products.push(newProduct);
   }
 }
